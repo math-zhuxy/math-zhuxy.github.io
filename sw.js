65a26f03a997d04a07d5f366fcbda943e203c799
math-zhuxy <235934429@qq.com>
2025-05-27 10:46:38 +0800

    Site updated: 2025-05-27 10:46:36

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 // js,css 缓存
-workbox.routing.registerRoute(
-    new RegExp('\\.(?:js|css)$'), // 路由匹配正则
-    new workbox.strategies.CacheFirst({ // 缓存优先策略
-        cacheName: 'JsCssCache',
-        plugins: [
-            new workbox.expiration.ExpirationPlugin({
-                maxEntries: 50, // 设置最大请求条数
-                maxAgeSeconds: 7 * 24 * 60 * 60, // 设置最长请求时间7天
-            }),
-        ],
-    })
-);
+// workbox.routing.registerRoute(
+//     new RegExp('\\.(?:js|css)$'), // 路由匹配正则
+//     new workbox.strategies.CacheFirst({ // 缓存优先策略
+//         cacheName: 'JsCssCache',
+//         plugins: [
+//             new workbox.expiration.ExpirationPlugin({
+//                 maxEntries: 50, // 设置最大请求条数
+//                 maxAgeSeconds: 7 * 24 * 60 * 60, // 设置最长请求时间7天
+//             }),
+//         ],
+//     })
+// );
 
 // // json 缓存
 // workbox.routing.registerRoute(
